111737d8d9adb38777a336716022c93be99b2a14
hihaho8 <pepijntur@gmail.com>
2026-02-20 22:37:14 +0100

    Add grid drawing and simulation functions
    Implement basic simulation with grid drawing.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+const TICK_RATE = 20;
+let running = false;
+
+function drawGrid() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "lime";
+  ctx.fillRect(100, 100, 50, 50);
+}
+
+function simulateTick() {
+  drawGrid();
+}
+
+function startSimulation() {
+  if (running) return;
+  running = true;
+
+  setInterval(() => {
+    simulateTick();
+  }, 1000 / TICK_RATE);
+}
